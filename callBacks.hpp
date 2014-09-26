ae98b1f900656e2fd12cc29072b5ea301231e9d1
adityakumarakash <adityakumarakash@gmail.com>
2014-09-26 19:42:01 +0530

    added the callBacks.hpp containing template

---
+++ callBacks.hpp
@@ -0,0 +1,32 @@
+#ifndef _CALLBACKS_HPP_
+#define _CALLBACKS_HPP_
+
+#include "gl_framework.hpp"
+#include "transformer_part.hpp"
+
+struct group_t{
+  
+  std::vector<body_t*> bodyList;             // the body for which this call back is defined
+  int ptr;                              // the current body
+  int totCount;                         //  all bodies
+  
+  //! constructor
+  group_t(void);
+
+  //! add a body to the List
+  void addBody(void);
+  
+  //! changing the bot
+  void nextBody(void);
+  
+  //! return current body
+  body_t* currentBody(void);
+
+};
+
+namespace bot_t{
+  //!GLFW keyboard callback
+  void key_callback(GLFWwindow*, int, int, int, int);
+};
+
+#endif
