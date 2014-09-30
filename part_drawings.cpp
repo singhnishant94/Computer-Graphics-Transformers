52b587e5b1f31179c076c0a4ca811c17510f4c0c
Nishant Kumar Singh <nishantkumarsingh1994@gmail.com>
2014-10-01 01:45:10 +0530

    Shapes of parts updated

--- part_drawings.cpp
+++ part_drawings.cpp
@@ -60,6 +60,55 @@ namespace drawing_t{
           //rotqube +=0.9f;                       // Increase Angle
   }
   
+
+  void drawChest(void){
+    
+          // Reset The Current Modelview Matrix
+          //glLoadIdentity();
+          //glScalef(1/4.0, 1/4.0, 1/4.0);
+          //NEW//////////////////NEW//////////////////NEW//////////////////NEW/////////////
+ 
+          //glTranslatef(0.0f, 0.0f,-7.0f);       // Translate Into The Screen 7.0 Units
+          //glRotatef(rotqube,0.0f,1.0f,0.0f);    // Rotate The cube around the Y axis
+          //glRotatef(rotqube,1.0f,1.0f,1.0f);
+          glBegin(GL_QUADS);            // Draw The Cube Using quads
+            //glColor3f(_r,_g,_b);
+            double y_val = 1.4f;
+            glVertex3f( 1.0f, y_val,-1.0f);      // Top Right Of The Quad (Top)
+            glVertex3f(-1.0f, 1.0f,-1.0f);      // Top Left Of The Quad (Top)
+            glVertex3f(-1.0f, 1.0f, 1.0f);      // Bottom Left Of The Quad (Top)
+            glVertex3f( 1.0f, y_val, 1.0f);      // Bottom Right Of The Quad (Top)
+            //glColor3f(1.0f,0.5f,0.0f);  // Color Orange
+            glVertex3f( 1.0f,-y_val, 1.0f);      // Top Right Of The Quad (Bottom)
+            glVertex3f(-1.0f,-1.0f, 1.0f);      // Top Left Of The Quad (Bottom)
+            glVertex3f(-1.0f,-1.0f,-1.0f);      // Bottom Left Of The Quad (Bottom)
+            glVertex3f( 1.0f,-y_val,-1.0f);      // Bottom Right Of The Quad (Bottom)
+            //glColor3f(1.0f,0.0f,0.0f);  // Color Red
+            glVertex3f( 1.0f, y_val, 1.0f);      // Top Right Of The Quad (Front)
+            glVertex3f(-1.0f, 1.0f, 1.0f);      // Top Left Of The Quad (Front)
+            glVertex3f(-1.0f,-1.0f, 1.0f);      // Bottom Left Of The Quad (Front)
+            glVertex3f( 1.0f,-y_val, 1.0f);      // Bottom Right Of The Quad (Front)
+            //glColor3f(1.0f,1.0f,0.0f);  // Color Yellow
+            glVertex3f( 1.0f,-y_val,-1.0f);      // Top Right Of The Quad (Back)
+            glVertex3f(-1.0f,-1.0f,-1.0f);      // Top Left Of The Quad (Back)
+            glVertex3f(-1.0f, 1.0f,-1.0f);      // Bottom Left Of The Quad (Back)
+            glVertex3f( 1.0f, y_val,-1.0f);      // Bottom Right Of The Quad (Back)
+            //glColor3f(1.0f,1.0f,1.0f);  // Color white
+            glVertex3f(-1.0f, 1.0f, 1.0f);      // Top Right Of The Quad (Left)
+            glVertex3f(-1.0f, 1.0f,-1.0f);      // Top Left Of The Quad (Left)
+            glVertex3f(-1.0f,-1.0f,-1.0f);      // Bottom Left Of The Quad (Left)
+            glVertex3f(-1.0f,-1.0f, 1.0f);      // Bottom Right Of The Quad (Left)
+            //glColor3f(1.0f,0.0f,1.0f);  // Color Violet
+            glVertex3f( 1.0f, y_val,-1.0f);      // Top Right Of The Quad (Right)
+            glVertex3f( 1.0f, y_val, 1.0f);      // Top Left Of The Quad (Right)
+            glVertex3f( 1.0f,-y_val, 1.0f);      // Bottom Left Of The Quad (Right)
+            glVertex3f( 1.0f,-y_val,-1.0f);      // Bottom Right Of The Quad (Right)
+            //glColor3f(1.0f,0.0f,0.0f);
+          glEnd();                      // End Drawing The Cube
+ 
+          //rotqube +=0.9f;                       // Increase Angle
+
+  }
   //! Now the functions to draw various diff body parts
   //! these functions push the various things in a list
   
@@ -71,7 +120,7 @@ namespace drawing_t{
   //! for Hip 
   void drawHip(int p_num, double len){ //! part number, length
     glNewList(p_num, GL_COMPILE);
-    glScalef(1.0f,0.25f,0.5f);
+    glScalef(1.5f,0.25f,0.5f);
     glColor3f(0.2f,0.2f,0.2f);
     drawCube();
     glColor3f(1.0f,1.0f,1.0f);
@@ -81,9 +130,23 @@ namespace drawing_t{
   //! for torso
   void drawTorso(int p_num, double len){ //! part number, length
     glNewList(p_num, GL_COMPILE);
-    glScalef(1.0f,0.85f,0.5f);
-    glColor3f(0.6f,0.6f,0.6f);
-    drawCube();
+    glPushMatrix();
+    glTranslatef(0.7f,0.0f,0.0f);
+    glScalef(0.8f,1.5f,0.5f);
+    glColor3f(0.4f,0.4f,0.9f);
+    drawChest();
+    glPopMatrix();
+
+    for (int i = 0; i < 4; ++i)
+    {
+      glPushMatrix();
+      glTranslatef(-1.55f+i/2.2,0.0f,0.0f);
+      glScalef(0.2f,0.4f,0.4f);
+      glColor3f(0.3f,0.3f,0.3f);
+      glRotatef(180,0,1,0);
+      drawChest();
+      glPopMatrix();
+    }
     glColor3f(1.0f,1.0f,1.0f);
     glEndList();
   }
@@ -91,9 +154,28 @@ namespace drawing_t{
   //! for shoulder
   void drawShoulder(int p_num, double len){ //! part number, length
     glNewList(p_num, GL_COMPILE);
-    glScalef(1.0f,0.25f,0.5f);
-    glColor3f(0.2f,0.2f,0.2f);
+    glPushMatrix();
+    glScalef(2.1f,0.5f,0.5f);
+    glColor3f(0.4f,0.4f,0.4f);
     drawCube();
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(1.4f,0.4f,0.0f);
+    glRotatef(-90,0,0,1);
+    glScalef(0.2f,0.4f,0.4f);
+    glColor3f(0.3f,0.3f,0.3f);
+    drawChest();
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(-1.4f,0.4f,0.0f);
+    glRotatef(-90,0,0,1);
+    glScalef(0.2f,0.4f,0.4f);
+    glColor3f(0.3f,0.3f,0.3f);
+    drawChest();
+    glPopMatrix();
+
     glColor3f(1.0f,1.0f,1.0f);
     glEndList();
   }
@@ -101,8 +183,8 @@ namespace drawing_t{
   //! for neck
   void drawNeck(int p_num, double len){ //! part number, length
     glNewList(p_num, GL_COMPILE);
-    glScalef(1.0f,0.2f,0.2f);
-    glColor3f(0.9f,0.9f,0.9f);
+    glScalef(1.0f,0.5f,0.2f);
+    glColor3f(0.3f,0.3f,0.6f);
     drawCube();
     glColor3f(1.0f,1.0f,1.0f);
     glEndList();
@@ -111,9 +193,25 @@ namespace drawing_t{
   //! for arm
   void drawArm(int p_num, double len){ //! part number, length
     glNewList(p_num, GL_COMPILE);
-    glScalef(1.0f,0.20f,0.20f);
-    glColor3f(0.2f,0.2f,0.2f);
+    glPushMatrix();
+    glTranslatef(0.4f,0.0f,0.0f);
+    glScalef(1.0f,0.40f,0.4f);
+    glColor3f(0.3f,0.3f,0.7f);
     drawCube();
+    glPopMatrix();
+
+    
+
+   
+
+    glPushMatrix();
+    glTranslatef(-0.8f,0.0f,0.0f);
+    glScalef(0.4f,0.4f,0.4f);
+    glColor3f(0.3f,0.3f,0.3f);
+    glRotatef(180,0,1,0);
+    drawChest();
+    glPopMatrix();
+
     glColor3f(1.0f,1.0f,1.0f);
     glEndList();  
   }
@@ -121,19 +219,44 @@ namespace drawing_t{
   //! for Hand
   void drawHand(int p_num, double len){ //! part number, length
     glNewList(p_num, GL_COMPILE);
-    glScalef(1.0f,0.18f,0.18f);
-    glColor3f(0.7f,0.7f,0.7f);
+    glPushMatrix();
+    glTranslatef(0.8f,0.0f,0.0f);
+    glScalef(1.0f,0.30f,0.30f);
+    glColor3f(0.5f,0.5f,0.9f);
     drawCube();
     glColor3f(1.0f,1.0f,1.0f);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(-0.3f,0.0f,0.0f);
+    glScalef(0.3f,0.3f,0.3f);
+    glColor3f(0.3f,0.3f,0.3f);
+    glRotatef(180,0,1,0);
+    drawChest();
+    glPopMatrix();
+
     glEndList();
   }
 
   //! for Thigh
   void drawThigh(int p_num, double len){ //! part number, length
     glNewList(p_num, GL_COMPILE);
-    glScalef(1.0f,0.25f,0.25f);
-    glColor3f(0.4f,0.4f,0.4f);
+    glPushMatrix();
+    glTranslatef(0.7f,0.0f,0.0f);
+    glScalef(1.2f,0.6f,0.6f);
+    glColor3f(0.4f,0.4f,0.8f);
     drawCube();
+    glPopMatrix();
+
+    
+
+    glPushMatrix();
+    glTranslatef(-0.8f,0,0);
+    glRotatef(0,0,1,0);
+    glScalef(0.4f,0.4f,0.4f);
+    glColor3f(0.3f,0.3f,0.3f);
+    drawChest();
+    glPopMatrix();
     glColor3f(1.0f,1.0f,1.0f);
     glEndList();
   }
@@ -141,9 +264,20 @@ namespace drawing_t{
   //! for Leg
   void drawLeg(int p_num, double len){ //! part number, length
     glNewList(p_num, GL_COMPILE);
-    glScalef(1.0f,0.2f,0.2f);
-    glColor3f(0.65f,0.65f,0.76f);
+    glPushMatrix();
+    glTranslatef(1.5f,0.0f,0.0f);
+    glScalef(1.2f,0.4f,0.4f);
+    glColor3f(0.45f,0.45f,0.76f);
     drawCube();
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(-0.1f,0,0);
+    glRotatef(180,0,1,0);
+    glScalef(0.4f,0.4f,0.4f);
+    glColor3f(0.3f,0.3f,0.3f);
+    drawChest();
+    glPopMatrix();
     glColor3f(1.0f,1.0f,1.0f);
     glEndList();  
   }
@@ -151,8 +285,9 @@ namespace drawing_t{
   //! for Foot 
   void drawFoot(int p_num, double len){ //! part number, length
     glNewList(p_num, GL_COMPILE);
-    glScalef(1.0f,0.2f,0.2f);
-    glColor3f(0.2f,0.2f,0.2f);
+    glTranslatef(-0.2f,-0.0f,-1.5f);
+    glScalef(1.0f,0.5f,0.3f);
+    glColor3f(0.6f,0.6f,1.0f);
     drawCube();
     glColor3f(1.0f,1.0f,1.0f);
     glEndList();
